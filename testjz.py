d074514033de2e5103c235436b82e36d08b3ccea
jzerez <Jonathan.zerez@students.olin.edu>
2020-12-13 13:26:48 -0500

    refactor bk-p

--- testjz.py
+++ testjz.py
@@ -23,6 +23,26 @@ def bk(g, r,p,x, depth=0):
         x = x.union([node])
 
 
+def bk_p(g,r,p,x, counter=0):
+    """
+    Bron-Kerbosch with pivots
+    """
+    pux = p.union(x)
+    if not pux:
+        print(r)
+        return r
+
+    pivot = next(iter(pux))
+    neighborsP = list(g.neighbors(pivot))
+    counter += 1
+    for v in p.difference(neighborsP):
+        neighborsV = list(g.neighbors(v))
+        bk_p(g, r.union([v]), p.intersection(neighborsV), x.intersection(neighborsV), counter)
+        p.remove(v)
+        x.add(v)
+
+
+
 # G = nx.generators.random_graphs.connected_watts_strogatz_graph(10, 5, 0.2, seed=420)
 # G = nx.generators.classic.complete_graph(3)
 G = nx.generators.classic.wheel_graph(5)
@@ -30,6 +50,7 @@ G = nx.generators.classic.wheel_graph(5)
 # G.add_edge(2,3)
 nx.draw_circular(G, with_labels=True)
 
-bk(G, set([]), set(G.nodes), set([]))
 
+bk(G, set([]), set(G.nodes), set([]))
+bk_p(G, set([]), set(G.nodes), set([]))
 plt.show()
